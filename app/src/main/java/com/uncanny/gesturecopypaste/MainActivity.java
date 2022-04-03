9f74055766adcb46e58bf002a162d4366b870bcc
uncannyRishabh <rishabhrajgupta2000@gmail.com>
2022-04-03 08:15:00 +0530

    Initial commit

---
+++ app/src/main/java/com/uncanny/gesturecopypaste/MainActivity.java
@@ -0,0 +1,19 @@
+package com.uncanny.gesturecopypaste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
